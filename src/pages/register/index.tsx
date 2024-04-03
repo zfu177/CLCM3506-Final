0e05a4a2534e5ccef53c66ca54963a7041494f2b
Zhongyi Fu <zyfu90@gmail.com>
2024-04-03 10:46:18 -0600

    cdk init

--- src/pages/register/index.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthPage } from "@refinedev/mui";
-
-export const Register = () => {
-  return <AuthPage type="register" />;
-};
---
+++ test/clcm3506-final.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Clcm3506Final from '../lib/clcm3506-final-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clcm3506-final-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Clcm3506Final.Clcm3506FinalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
